eb6dfb0eed1e91100b9cc50322b081451f8827f2
I. Bugra Aslan <46376529+plaistocene@users.noreply.github.com>
2022-06-18 04:55:11 +0300

    Network Request added to MyListFragment.java and Network.java deleted

--- app/src/main/java/com/plaistocene/animewatchlist/MyListFragment.java
+++ app/src/main/java/com/plaistocene/animewatchlist/MyListFragment.java
@@ -1,21 +1,26 @@
 package com.plaistocene.animewatchlist;
 
-import static com.plaistocene.animewatchlist.Network.PostmanNetworkRequestResult.*;
-
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.gson.Gson;
+import com.plaistocene.animewatchlist.Entities.MovieInfoModifications;
 import com.plaistocene.animewatchlist.Model.MovieInfo;
 import com.plaistocene.animewatchlist.Model.Result;
-import com.plaistocene.animewatchlist.Network.PostmanNetworkRequestResult;
+import com.plaistocene.animewatchlist.Network.RetrofitRequestInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MyListFragment extends Fragment
@@ -23,25 +28,63 @@ public class MyListFragment extends Fragment
     View view;
     MovieInfo movieInfo;
     Result[] results;
+    RecyclerView recyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
+
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_my_list, container, false);
 
-        RecyclerView recyclerView = view.findViewById(R.id.my_list_rc);
-
-        createTestValsForRC();
+        recyclerView = view.findViewById(R.id.my_list_rc);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
-        recyclerView.setAdapter(new ListAdapter(results));
+
+
+        allAnimeRequest();
+
 
         return view;
     }
 
-    public void createTestValsForRC(){
-        Gson gson = new Gson();
-        movieInfo = gson.fromJson(network_request_result, MovieInfo.class);
-        results = movieInfo.getResults().toArray(new Result[0]);
+    public void allAnimeRequest()
+    {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://imdb-api.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitRequestInterface retrofitRequestInterface = retrofit.create(RetrofitRequestInterface.class);
+
+        Call<MovieInfo> movieInfoCall = retrofitRequestInterface.getAllAnime();
+
+        movieInfoCall.enqueue(new Callback<MovieInfo>()
+        {
+            @Override
+            public void onResponse(Call<MovieInfo> call, Response<MovieInfo> response)
+            {
+                MovieInfoModifications mim = new MovieInfoModifications();
+
+                if (response.isSuccessful())
+                {
+                    movieInfo = response.body();
+                    System.out.println(movieInfo);
+                    results = mim.getResultsFromMovieInfo(movieInfo);
+
+                    recyclerView.setAdapter(new ListAdapter(results));
+                } else
+                {
+                    Log.e("error", "the network call on allAnimeRequest Failed");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MovieInfo> call, Throwable t)
+            {
+                Log.e("error", "the network call on allAnimeRequest Failed");
+            }
+        });
+//    }
     }
 }
--- app/src/main/java/com/plaistocene/animewatchlist/Network/Network.java
+++
@@ -1,6 +0,0 @@
-package com.plaistocene.animewatchlist.Network;
-
-public class Network
-{
-
-}
